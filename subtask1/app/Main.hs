6d6eb0df78773f444643f242ec51169f38a36c34
Jitendra2603 <140426495+Jitendra2603@users.noreply.github.com>
2024-09-23 21:58:53 +0530

    Update Main.hs

--- subtask1/app/Main.hs
+++ subtask1/app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Editor.UI as UI
@@ -7,14 +8,33 @@ import qualified Editor.MultiBuffer as MultiBuffer
 import qualified Config.Settings as Settings
 import Control.Monad.State
 import System.Environment (getArgs)
+import Control.Exception (catch, SomeException)
+import System.IO (hPutStrLn, stderr)
+import Data.Text (Text)
+import qualified Data.Text as T
+import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
     args <- getArgs
+    settings <- Settings.loadSettings `catch` handleConfigError
     initialState <- case args of
-        [] -> return $ MultiBuffer.emptyState
-        (file:_) -> do
-            content <- FileIO.readFile file
-            return $ MultiBuffer.initialState file content
-    settings <- Settings.loadSettings
+        [] -> return MultiBuffer.emptyState
+        (file:_) -> loadFile file `catch` handleFileError
     evalStateT (UI.runEditor settings) initialState
+  where
+    loadFile :: FilePath -> IO MultiBuffer.EditorState
+    loadFile file = do
+        content <- FileIO.readFile file
+        return $ MultiBuffer.initialState file content
+
+    handleConfigError :: SomeException -> IO Settings.Settings
+    handleConfigError e = do
+        hPutStrLn stderr $ "Error loading config: " ++ show e
+        hPutStrLn stderr "Using default settings"
+        return Settings.defaultSettings
+
+    handleFileError :: SomeException -> IO MultiBuffer.EditorState
+    handleFileError e = do
+        hPutStrLn stderr $ "Error loading file: " ++ show e
+        exitFailure
